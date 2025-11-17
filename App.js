ddfdf265d491bee4e6607f2221f6495c427fefab
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-11-17 10:48:15 +0100

    feat: Implement props-based data flow between React Native components

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LoginForm from "./src/components/LoginForm";
+import Saludar from "./src/components/Saludar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LoginForm />
+      <Text>Curso React Native</Text>
+      <Saludar name="Vegeta" />
+      <Saludar name="!" />
+      <Saludar name="Bulma" />
+      <Saludar name="Goku" />
     </View>
   );
 }
